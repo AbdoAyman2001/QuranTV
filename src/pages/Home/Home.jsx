3f5df06f46e74cbcab194800d990d930f6b172e6
Abdul-Rahman Ayman <77454535+AbdoAyman2001@users.noreply.github.com>
2023-03-19 14:27:55 +0200

    added home page and changed font family

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,31 +1,34 @@
 import React, { useEffect } from "react";
-import { useNavigate, useNavigation } from "react-router-dom";
+import { useLocation, useNavigate, useNavigation } from "react-router-dom";
 import classes from "./Home.module.css";
+import quran from "../../assets/vector.jpg";
 
 const Home = () => {
-  //////////////////////// Temp code ////////////////
   const navigate = useNavigate();
-  useEffect(() => {
-    const timeout = setTimeout(() => {
-      navigate("/reciters");
-    }, 1000);
-
-    return () => {
-      clearTimeout(timeout);
-    };
-  }, []);
-  /////////////////////////////////////////
-
   return (
     <>
-      {/* <section className={classes.hero}>
-        <div className={classes.details}>
-          <h1 className={classes.title}></h1>
-          <h3 className={classes[`sub-title`]}></h3>
-          <div className={classes.action}></div>
+      <section className={classes.hero}>
+        <div className={classes.container}>
+          <div className={classes.details}>
+            <h1 className={classes.title}>
+              {" "}
+              وَإِذَا قُرِئَ الْقُرْآنُ فَاسْتَمِعُوا لَهُ وَأَنصِتُوا
+              <strong> لَعَلَّكُمْ تُرْحَمُونَ</strong>{" "}
+            </h1>
+            <h3 className={classes[`sub-title`]}>
+              فَكُنْ مَع قَوَافِلِ المَرْحُومِينَ
+            </h3>
+            <div className={classes.action}>
+              <button onClick={() => navigate("/reciters")}>
+                قائمة القراء
+              </button>
+            </div>
+          </div>
+          <div className={classes.visuals}>
+            <img src={quran} alt="quran vector" />
+          </div>
         </div>
-
-      </section> */}
+      </section>
     </>
   );
 };
